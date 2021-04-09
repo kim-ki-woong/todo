8c6fca9ba9366c024a090da60331f47d8bae63d0
{KIM} <{kko2746@nate.com}>
2021-04-09 19:18:18 +0900

    first commit

---
+++ todoSubject_restfulAPI/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.conf.urls import include, url
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('todo_subject_restful_main.urls')),
+]
